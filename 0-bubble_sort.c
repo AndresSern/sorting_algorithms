cc639c285ef19e8a99cfa7a52fbe883cad669d1f
AndresSern <andrecamposer@gmail.com>
2021-02-01 20:08:27 +0000

    Changing the file by copy-buble.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,48 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
-void swap_Array(int *array, size_t j, size_t k);
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * @array: array of integer
- * @size: size of the array
- * int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+ *   main - Entry point
+ *   
+ *   Return: Always 0
  */
-void bubble_sort(int *array, size_t size)
-{
-	size_t idx = 0, aux = 0, loop = 0;
+ 
 
-	while (idx < size)
+ void bubble_sort(int *array, size_t size)
+{
+	size_t a, b;
+	size_t n = size;
+	int temp = 0;
+
+	int *arrayB = malloc(n * sizeof(int));
+	
+	arrayB = array;
+	
+	for (a = 1; a < n; a++)
 	{
-		aux = idx + 1;
-		if (aux < size && array[aux] < array[idx])
+		for(b = 0; b < n - 1; b++)
 		{
-			swap_Array(array, idx, idx + 1);
-			print_array(array, size);
-			loop++;
+			if (arrayB[b] > arrayB[b + 1])
+			{
+				temp = arrayB[b];
+				arrayB[b] = arrayB[b + 1];
+				arrayB[b + 1] = temp;
+				print_array(arrayB, n);
+			}
 		}
-		if (idx + 1 == size)
-		{
-			if (loop == 0)
-				break;
-			idx = 0;
-			loop = 0;
-			continue;
-		}
-		idx += 1;
 	}
-}
-
-/**
- * swap_Array - Swap two element of position in an array
- * @array: Point to array
- * @j: Position To do the Swap
- * @k: Position to do the swap
- */
-void swap_Array(int *array, size_t j, size_t k)
-{
-	int temp;
-
-	temp = array[j];
-	array[j] = array[k];
-	array[k] = temp;
 }
